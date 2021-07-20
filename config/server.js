e8fdbc4bff56e1b3d3e29c31cc8e70f553895d57
bucketheadland666 <85567892+bucketheadland666@users.noreply.github.com>
2021-07-19 20:48:33 -0500

    Add files via upload

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a8d04e863b4c8a93a3dd07cde868b040'),
+    },
+  },
+});
